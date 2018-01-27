888ba518847d0edf6b31405fb894cae2f7847f36
jpmadeira <contato.julianomadeira@gmail.com>
2018-01-27 17:16:18 -0200

    Madeiracash

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x16, 0x1e, 0x53, 0x6f, 0x41, 0x55, 0x18, 0x44, 0x2f, 0x11, 0x0e, 0x51, 0x48, 0x12, 0x0f, 0x26 } };
+}
